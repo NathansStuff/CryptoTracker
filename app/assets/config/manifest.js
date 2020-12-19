af70c82a371cfa3bf8b628a57737367556c8ac18
Nathanstuff <n.odonnell@hotmail.com>
2020-12-19 14:54:55 +1100

    purchase form styled

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,17 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+
+
+
+// const purchaseContainer = document.getElementById('purchase-container')
+
+// purchaseContainer.addEventListener('keydown', () => {
+//     const totalCost = document.getElementById('purchaseTotal')
+//     const cryptoPrice = document.getElementById('purchasePrice')
+//     const purchaseAmound = document.getElementById('purchaseAmound')
+//     const purchaseFee = document.getElementById('purchaseFee')
+    
+//     totalCost.innerHTML = cryptoPrice.innerHTML
+//     console.log('ssss********************************')
+// })
